cf3a5ad59a310196894483c679b580e44070ed41
beyzakisla <120708014+beyzakisla@users.noreply.github.com>
2024-04-07 23:33:05 +0300

    add files

---
+++ beyzasite/beyzasite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using beyzasite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace beyzasite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Ɛ> Welcome my friend <3";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
